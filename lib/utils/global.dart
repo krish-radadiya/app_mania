b3db5636dd53c3154525057a9cc53176d9b611ff
HP <radadiyakrish62@gmail.com>
2023-09-20 21:42:07 +0530

    first commit

---
+++ lib/utils/global.dart
@@ -0,0 +1,305 @@
+class Global {
+  static List<Map<String, dynamic>> allfoods = [
+    {
+      'categoryName': 'Vez Pizza',
+      'categoryFoods': [
+        {
+          'id': 1,
+          'name': 'DOUBLE CHEESE MARGHERITA',
+          'time': '20min',
+          'price': 299,
+          'ratting': 4.2,
+          'discount': 14,
+          'thumbnail':
+              "https://www.dominos.co.in/files/items/Double_Cheese_Margherita.jpg",
+          'discription':
+              "The ever-popular Margherita - loaded with extra cheese... oodies of it!",
+        },
+        {
+          'id': 2,
+          'name': 'CHEESE N CORN',
+          'time': '20min',
+          'price': 399,
+          'ratting': 4.5,
+          'discount': 12,
+          'thumbnail':
+              "https://www.dominos.co.in/files/items/Corn_&_Cheese.jpg",
+          'discription': "Cheese I Golden Corn | Cheese n Corn Pizza",
+        },
+        {
+          'id': 3,
+          'name': 'VEG EXTRAVAGANZA',
+          'time': '20min',
+          'price': 499,
+          'ratting': 4.1,
+          'discount': 20,
+          'thumbnail':
+              "https://www.dominos.co.in/files/items/Veg_Extravaganz.jpg",
+          'discription':
+              "A pizza that decidedly staggers under an overload of golden corn, exotic black olives, crunchy onions, crisp capsicum, succulent mushrooms, juicyfresh tomatoes and jalapeno - with extra cheese to go all around.",
+        },
+        {
+          'id': 4,
+          'name': 'DELUXE VEGGIE',
+          'time': '20min',
+          'price': 299,
+          'ratting': 4.9,
+          'discount': 18,
+          'thumbnail':
+              "https://www.dominos.co.in/files/items/Deluxe_Veggie.jpg",
+          'discription':
+              "For a vegetarian looking for a BIG treat that goes easy on the spices, this one's got it all.. The onions, the capsicum, those delectable mushrooms - with paneer and golden corn to top it all.",
+        },
+        {
+          'id': 5,
+          'name': 'MEXICAN GREEN WAVE',
+          'time': '20min',
+          'price': 345,
+          'ratting': 5,
+          'discount': 16,
+          'thumbnail':
+              "https://www.dominos.co.in/files/items/Mexican_Green_Wave.jpg",
+          'discription':
+              "A pizza loaded with crunchy onions, crisp capsicum, juicy tomatoes and jalapeno with a liberal sprinkling of exotic Mexican herbs.",
+        },
+        {
+          'id': 6,
+          'name': 'PEPPY PANEER',
+          'time': '20min',
+          'price': 400,
+          'ratting': 4.8,
+          'discount': 14,
+          'thumbnail': "https://www.dominos.co.in/files/items/Peppy_Paneer.jpg",
+          'discription':
+              "Chunky paneer with crisp capsicum and spicy red pepper - quite a mouthful!",
+        },
+      ],
+    },
+    {
+      'categoryName': ' Non Vez Pizza',
+      'categoryFoods': [
+        {
+          'id': 1,
+          'name': 'CHICKEN FIESTA',
+          'time': '20min',
+          'price': 299,
+          'ratting': 4.8,
+          'discount': 12,
+          'thumbnail':
+              "https://www.dominos.co.in/files/items/MicrosoftTeams-image_(10).png",
+          'discription':
+              "Grilled Chicken Rashers I Peri-Peri Chicken I Onion I Capsicum",
+        },
+        {
+          'id': 2,
+          'name': 'CHICKEN PEPPERONI',
+          'time': '20min',
+          'price': 445,
+          'ratting': 4.3,
+          'discount': 18,
+          'thumbnail':
+              "https://www.dominos.co.in/files/items/MicrosoftTeams-image_(20).png",
+          'discription':
+              "A classic American taste! Relish the delectable flavor of Chicken Pepperoni, topped with extra cheese",
+        },
+        {
+          'id': 3,
+          'name': 'Chicken Sausage',
+          'time': '20min',
+          'price': 395,
+          'ratting': 4.6,
+          'discount': 14,
+          'thumbnail':
+              "https://www.dominos.co.in/files/items/MicrosoftTeams-image_(17).png",
+          'discription': "Chicken Sausage & Cheese",
+        },
+        {
+          'id': 4,
+          'name': 'PEPPER BARBECUE & ONION',
+          'time': '20min',
+          'price': 595,
+          'ratting': 4.9,
+          'discount': 15,
+          'thumbnail':
+              "https://www.dominos.co.in/files/items/Pepper_Barbeque_&_Onion.jpg",
+          'discription': "Pepper Barbecue Chicken I Onion",
+        },
+        {
+          'id': 5,
+          'name': 'CHICKEN DOMINATOR',
+          'time': '20min',
+          'price': 299,
+          'ratting': 4,
+          'discount': 8,
+          'thumbnail':
+              "https://www.dominos.co.in/files/items/MicrosoftTeams-image_(11).png",
+          'discription':
+              "Treat your taste buds with Double Pepper Barbecue Chicken, Peri-Peri Chicken, Chicken Tikka & Grilled Chicken Rashers",
+        },
+        {
+          'id': 6,
+          'name': 'CHICKEN GOLDEN DELIGHT',
+          'time': '20min',
+          'price': 345,
+          'ratting': 4.8,
+          'discount': 12,
+          'thumbnail':
+              "https://www.dominos.co.in/files/items/MicrosoftTeams-image_(14).png",
+          'discription':
+              "Mmm! Barbeque chicken with a topping of golden corn loaded with extra cheese. Worth its weight in gold!",
+        },
+      ],
+    },
+    {
+      'categoryName': 'PIZZA MANIA',
+      'categoryFoods': [
+        {
+          'id': 1,
+          'name': 'VEG LOADED',
+          'time': '20min',
+          'price': 299,
+          'ratting': 5,
+          'discount': 14,
+          'thumbnail': "https://www.dominos.co.in/files/items/PrimeLoadedL.jpg",
+          'discription':
+              "Tomato | Grilled Mushroom |Jalapeno |Golden Corn | Beans in a fresh pan crust",
+        },
+        {
+          'id': 2,
+          'name': 'CHEESY',
+          'time': '20min',
+          'price': 399,
+          'ratting': 4.8,
+          'discount': 20,
+          'thumbnail': "https://www.dominos.co.in/files/items/PrimeCheesyL.jpg",
+          'discription': "Orange Cheddar Cheese I Mozzarella",
+        },
+        {
+          'id': 3,
+          'name': 'PANEER & ONION',
+          'time': '20min',
+          'price': 499,
+          'ratting': 4.2,
+          'discount': 14,
+          'thumbnail':
+              "https://www.dominos.co.in/files/items/Paneer_Special.jpg",
+          'discription': "Creamy Paneer I Onion",
+        },
+        {
+          'id': 4,
+          'name': 'CAPSICUM',
+          'time': '20min',
+          'price': 299,
+          'ratting': 4.1,
+          'discount': 16,
+          'thumbnail': "https://www.dominos.co.in/files/items/CapsicumVeg.jpg",
+          'discription': "Capsicum",
+        },
+        {
+          'id': 5,
+          'name': 'CHEESE N TOMATO',
+          'time': '20min',
+          'price': 345,
+          'ratting': 4.3,
+          'discount': 12,
+          'thumbnail':
+              "https://www.dominos.co.in/files/items/cheese_and_tomato.png",
+          'discription': "A delectable combination of cheese and juicy tomato",
+        },
+        {
+          'id': 6,
+          'name': 'GOLDEN CORN',
+          'time': '20min',
+          'price': 400,
+          'ratting': 4.6,
+          'discount': 15,
+          'thumbnail':
+              "https://www.dominos.co.in/files/items/golden_corn_veg.jpg",
+          'discription': "Golden Corn - Sweet Corn Pizza",
+        },
+      ],
+    },
+    {
+      'categoryName': 'Cold Drinks',
+      'categoryFoods': [
+        {
+          'id': 1,
+          'name': 'Coke',
+          'time': '20min',
+          'price': 40,
+          'ratting': 4.4,
+          'discount': 4,
+          'thumbnail':
+              "https://4.imimg.com/data4/NG/JC/MY-30165244/cold-drink-500x500.jpg",
+          'discription':
+              "We are engrossed in presenting an inclusive gamut of Cold Drinks. Which are processed using high grade natural ingredients and reliable methodologies.",
+        },
+        {
+          'id': 2,
+          'name': 'Mojito',
+          'time': '20min',
+          'price': 69,
+          'ratting': 4.8,
+          'discount': 12,
+          'thumbnail':
+              "https://c8.alamy.com/comp/2G26GPA/mojito-cocktail-in-a-plastic-glass-with-a-tube-on-a-yellow-background-2G26GPA.jpg",
+          'discription':
+              "Mojito cocktail in a plastic glass with a tube on a yellow background Stock Photo - Alamy",
+        },
+        {
+          'id': 3,
+          'name': 'Cocktail Mojito',
+          'time': '20min',
+          'price': 100,
+          'ratting': 4.6,
+          'discount': 14,
+          'thumbnail':
+              "https://thumbs.dreamstime.com/b/summer-mint-strawberry-refreshing-cocktail-mojito-ice-plastic-glass-straw-carbonated-mineral-water-glass-drinks-276497618.jpg",
+          'discription':
+              "591 Cocktail Mojito Plastic Glass Stock Photos - Free & Royalty-Free Stock Photos from Dreamstime",
+        },
+        {
+          'id': 4,
+          'name': 'Colorful Cocktail',
+          'time': '20min',
+          'price': 99,
+          'ratting': 4.7,
+          'discount': 18,
+          'thumbnail':
+              "https://previews.123rf.com/images/thaweerat/thaweerat1705/thaweerat170500003/78896651-colorful-cocktail-or-tropical-mocktail-plastic-glass-of-energy-drink-with-lemonade-orange-juice.jpg",
+          'discription':
+              "Colorful Cocktail or Tropical Mocktail - Plastic glass of Energy Drink with lemonade, orange juice and watermelon syrup on wooden table and on blurred background.",
+        },
+        {
+          'id': 5,
+          'name': 'Light pink rose cocktail',
+          'time': '20min',
+          'price': 199,
+          'ratting': 5,
+          'discount': 16,
+          'thumbnail':
+              "https://previews.123rf.com/images/roxiller/roxiller2005/roxiller200500142/146731154-summer-refreshment-drinks-light-pink-rose-cocktail-with-rose-wine-on-a-white-table-with-rose-flowers.jpg",
+          'discription':
+              "Summer refreshment drinks. Light pink rose cocktail, with rose wine on a white table. With rose flowers. Copy space Refreshing pink cocktail in a glass on a white wooden background",
+        },
+      ],
+    },
+  ];
+  static Set addFav = {};
+  static List favData = addFav.toList();
+}
+
+List<Map<String, dynamic>> cartfoods = [];
+
+buyfoodsPrice() {
+  double sum = 0;
+  cartfoods.forEach((e) {
+    sum = sum + e['price'];
+  });
+  return sum;
+}
+
+lengthCart() {
+  num cartLength = cartfoods.length;
+  return cartLength;
+}
